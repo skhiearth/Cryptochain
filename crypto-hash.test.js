97a5f9a03800f3bdc7bb65fb326fd7890de8389d
Utkarsh Sharma <skhiearth@gmail.com>
2020-01-19 01:27:29 +0530

    generate hash using sha256

---
+++ crypto-hash.test.js
@@ -0,0 +1,15 @@
+const cryptoHash = require('./crypto-hash');
+
+describe('cryptoHash()', () => {
+    fcde2b2edba56bf408601fb721fe9b5c338d10ee429ea04fae5511b68fbf8fb9
+
+    it('generates a SHA-256 hashed output', () => {
+        expect(cryptoHash('bar'))
+        .toEqual('fcde2b2edba56bf408601fb721fe9b5c338d10ee429ea04fae5511b68fbf8fb9')
+    });
+
+    it('produces same hash for same input arguments', () => {
+        expect(cryptoHash('one', 'two', 'three'))
+            .toEqual(cryptoHash('two', 'one', 'three'))
+    });
+});
