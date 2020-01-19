0e6c6765464064071519ec60e61b7bb5fa4b88f5
Utkarsh Sharma <skhiearth@gmail.com>
2020-01-19 20:35:54 +0530

    pubsub credentials

---
+++ pubsub.js
@@ -0,0 +1,5 @@
+const credentials = {
+    publishKey: 'pub-c-3701a97d-ace8-4fb0-a0ec-b98156932e24',
+    subscribeKey: 'sub-c-a34f01a0-3acc-11ea-be28-ae0ede4a022d',
+    secretKey: 'sec-c-MzA2YTJmYTktODc4OC00MTM1LThhYjQtOTIyZWNjNGZlNDhk'
+};
