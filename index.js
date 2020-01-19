84880418a55f6b7eb9ba08eb50f90e821d04f439
Utkarsh Sharma <skhiearth@gmail.com>
2020-01-19 21:19:13 +0530

    sync chain on connect

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const request = require('request');
 const Blockchain = require('./blockchain');
 const bodyParser = require('body-parser');
 const PubSub = require('./pubsub');
-const PORT = 3000;
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({blockchain});
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 app.use(bodyParser.json());
 
@@ -19,9 +22,30 @@ app.post('/api/mine', (req, res) => {
 
     blockchain.addBlock({data});
 
+    pubsub.broadcastChain();
+
     res.redirect('/api/blocks');
 });
 
+const syncChains = () => {
+    request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+        if(!error && response.statusCode === 200){
+            const rootChain = JSON.parse(body);
+
+            blockchain.replaceChain(rootChain);
+        }
+    });
+};
+
+let PEER_PORT;
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+if(process.env.GENERATE_PEER_PORT == 'true'){
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+};
+
 app.listen(PORT, () => {
     console.log(`Listening at localhost:${PORT}`);
+    syncChains();
 });
